e9668b08615562f18965161e1ca834f81a7bb515
Fanor <mickaelvinet79@gmail.com>
2016-08-09 01:37:56 +0200

    Add cmd : ./uplmg -f <file to upload>

---
+++ uplmg.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# conding: utf-8
+
+import requests
+import sys, getopt
+
+# fonction principal
+def main(argv):
+	
+	inputfile = ""
+
+	#Si la longuer de l'argument est egale a 1 on exit
+	if len(sys.argv) == 1 :
+		print './uplmg.py -f <file to upload>'
+		sys.exit()
+
+	#On essaye de recuper les parametres
+	try:
+		opts, args = getopt.getopt(argv, 'hf:', ["ffile="])
+	
+	except getopt.GetoptError:
+		print './uplmg.py -f <file to upload>'
+		sys.exit(2)
+
+	#Pour chauque argument dans la commande
+	for opt, arg in opts:
+		#Si c'est -h on envoie l'aide
+		if opt == '-h':
+			print './uplmg.py -f <file to upload>'
+			sys.exit()
+		#SI c'est -f ou --ffile on upload le fichier
+		elif opt in ("-f", "--ffile"):
+			inputfile = arg
+
+			files = { 'file' : open(inputfile , 'rb') }
+			data = { 'senderid' : 'cmdUplmg' }
+			r = requests.post('https://uplmg.com/file/upload', files=files, data=data)
+			print r.text
+			sys.exit()
+
+	#print 'File to upload : ', inputfile
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
