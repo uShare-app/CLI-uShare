49383128ed120b68d38f2337a306129e892d2865
Fanor <mickaelvinet79@gmail.com>
2016-08-09 15:02:51 +0200

    Update uploadFile for read all file !

--- api.py
+++ api.py
@@ -1,17 +1,25 @@
 import requests
 import json
+import mimetypes
 
 urlApi = 'https://uplmg.com/'
 
 # Upload a file
 # file : path of the file
 def uploadFile(file):
-	files = { 'file' : ( 'capture.png', open(file , 'rb'), 'image/png' ) }
+
+	mimetypes.init()
+	filename = file.split('/')[-1]
+
+	mime = mimetypes.types_map['.' + filename.split('.')[-1]]
+	
+	files = { 'file' : ( filename, open(file , 'rb'), mime ) }
 	data = { 'senderid' : 'cmdUplmg' }
+	
 	r = requests.post(urlApi + 'file/upload', files=files, data=data)
 	print r.text
 
-#Get all header
+#Get headers
 def showHeaders(shortname):
 	r = requests.head(urlApi + shortname)
 	for nom, valeur in r.headers.items():
