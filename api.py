fe7f9f6c79ba416c191f94cdb84fe88ba161685a
Fanor <mickaelvinet79@gmail.com>
2016-08-09 14:30:26 +0200

    Change the result of show Headers

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ urlApi = 'https://uplmg.com/'
 # Upload a file
 # file : path of the file
 def uploadFile(file):
-	files = { 'file' : ( 'chest.png', open(file , 'rb'), 'image/png' ) }
+	files = { 'file' : ( 'capture.png', open(file , 'rb'), 'image/png' ) }
 	data = { 'senderid' : 'cmdUplmg' }
 	r = requests.post(urlApi + 'file/upload', files=files, data=data)
 	print r.text
@@ -14,4 +14,6 @@ def uploadFile(file):
 #Get all header
 def showHeaders(shortname):
 	r = requests.head(urlApi + shortname)
-	print r.headers
+	for nom, valeur in r.headers.items():
+		if nom.startswith('Uplmg'):
+			print nom + ": " + valeur
