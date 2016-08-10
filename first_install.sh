3ecb7e0d847467568ed1622392a98b797290dc4f
Fanor <mickaelvinet79@gmail.com>
2016-08-10 23:40:55 +0200

    Add Script for First Install

---
+++ first_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt install python3
+
+if [ -f "get-pip.py" ]; then
+	echo 'Already download !'
+else
+	wget https://bootstrap.pypa.io/get-pip.py && python get-pip.py
+fi
+
+requests=`pip list | grep "requests"`
+tabulate=`pip list | grep "tabulate"`
+if [ -z "$requests" ]; then
+	pip install requests
+fi
+if [ -z "$tabulate" ]; then
+	pip install tabulate
+fi
+
