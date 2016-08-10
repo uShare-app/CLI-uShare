1fb76b359f6c584925747a51462417be90da4396
Fanor <mickaelvinet79@gmail.com>
2016-08-10 21:33:28 +0200

    Add Table

--- api/search.py
+++ api/search.py
@@ -3,11 +3,34 @@ import mimetypes
 import requests
 import sys
 
+from tabulate import tabulate
+
 #Show Search
 def showSearch( urlApi ):
+	headers = ['mimetype', 'extension', 'encoding', 'views', 'senderid', 'shortName', 'originalFileName', 'size']
+	datas = []
+
 	r = requests.get( urlApi + '/api/files/search' )
 	search = json.loads( r.text )
 	for i in search:
+		data = []
 		for name, value in i.items():
-			if name == "shortName":
-				print name + ": " + value
+			if name == 'senderid':
+				data.append(value)
+			if name == 'size':
+				data.append(value)
+			if name == 'mimetype':
+				data.append(value)
+			if name == 'encoding':
+				data.append(value)
+			if name == 'originalFileName':
+				data.append(value)
+			if name == 'shortName':
+				data.append(value)
+			if name == 'extension':
+				data.append(value)
+			if name == 'views':
+				data.append(value)
+		datas.append(data)
+
+	print(tabulate(datas, headers=headers, tablefmt='orgtbl'))
